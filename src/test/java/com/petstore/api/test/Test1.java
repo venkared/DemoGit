951a1e6b082b389d74325d04ffa17eb254aa88f1
kvrre <kvrre@Venkat>
2024-01-20 08:33:23 +0530

    Test1 method has been added

---
+++ src/test/java/com/petstore/api/test/Test1.java
@@ -0,0 +1,15 @@
+package com.petstore.api.test;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	
+	
+	@Test
+	public void Test12()
+	{
+		System.out.println("This Test12 method for Testing");
+	}
+
+}
